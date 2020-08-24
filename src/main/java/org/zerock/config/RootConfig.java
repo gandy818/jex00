9b5729988a419a11b6deefec00524bc5803fab74
gandy818 <gandy818@gmail.com>
2020-08-24 15:37:28 +0900

    .

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,8 +1,71 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+//클래스에 component를 붙이면 bean이 된다.
+
 @Configuration
+@ComponentScan(basePackages = "org.zerock.sample")
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig {
 
+	
+// bean 나눠쓰는법
+	
+	@Bean()
+	public HikariConfig hikariConfig() {
+		HikariConfig hikariConfig = new HikariConfig();
+//		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		return hikariConfig;
+	} 
+	
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource(HikariConfig hikariConfig) {
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		
+		return bean;
+	}
+	
+	
+//bean 합쳐서 쓰는법
+	
+	/*
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+	
+		return dataSource;
+	}
+	*/
+	
 }
